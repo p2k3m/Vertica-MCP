f1e1314802ce12b9dd62d055d6696f9bdacfce1d
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2025-10-19 20:37:25 +0530

    Add SQL query to get event related CI details

---
+++ sql/get_event_ci.sql
@@ -0,0 +1,7 @@
+SELECT e.event_id,
+e.related_ci_display_label AS ci_name,
+e.related_ci_cmdb_id AS ci_cmdb_id,
+e.related_ci_cmdb_global_id,
+e.related_ci_type
+FROM :schema.opr_event e
+LIMIT :limit;
