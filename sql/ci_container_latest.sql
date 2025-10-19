96c428500deedbd01cbbcd6fe60190c000fcb0ce
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2025-10-19 20:29:44 +0530

    Add SQL query to fetch latest container metrics

---
+++ sql/ci_container_latest.sql
@@ -0,0 +1,5 @@
+SELECT resource_name AS container_name, pod_name, cpu_limit_util_pct_mean, mem_limit_util_pct_mean, restart_count_rate, timestamp_utc_end_s
+FROM :schema.cloud_gcp_gke_container
+WHERE cmdb_id = :ci_id
+ORDER BY timestamp_utc_end_s DESC
+LIMIT 1;
